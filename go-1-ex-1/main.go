2b45b0a5d9bdffe7b7e4f9a2c8c83930cfddd977
Nico Marcuard <Nico_Marcuard@sluz.ch>
2024-11-18 09:22:55 +0100

    EX 1 and EX 2

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,25 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	//Print für Vor- und Nachname
+	var firstName string = "Nico"
+	var lastName string = "Marcuard"
+
+	//Print für Geburtsdatum
+	var dayOfBirth int = 9
+	var monthOfBirth int = 1
+	var yearOfBirth int = 2007
+
+	//Print für Geschwister
+	var numberOfSiblings int = 0
+
+	//Print für Grösse
+	var heightInMeters float32 = 1.88
+
+	//Print für Strenzeichen
+	var zodiacSign rune = '\u2651'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
