0a7af65c85a81da849f1baa7cfdeadecf73c1298
Nico Marcuard <Nico_Marcuard@sluz.ch>
2024-11-25 09:11:27 +0100

    go-2-ex-2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -6,10 +6,14 @@ func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
 	fibs[2] = fibs[0] + fibs[1]
-	// TODO: correct up to index 4 using direct element access
+	fibs[3] = fibs[1] + fibs[2]
+	fibs[4] = fibs[2] + fibs[3]
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
-	// TODO: compute three more Fibonacci numbers and append them
+	fibs = append(fibs, fibs[3]+fibs[4]) // Hier rechne ich die 3 Zahl und die 4 Zahl zussamenn um die nächste zu bekommen
+
+	fibs = append(fibs, fibs[len(fibs)-2]+fibs[len(fibs)-1]) // muss 13 sein
+	fibs = append(fibs, fibs[len(fibs)-2]+fibs[len(fibs)-1]) // muss 21 sein
+	fibs = append(fibs, fibs[len(fibs)-2]+fibs[len(fibs)-1]) // muss 34 sein
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
 }
