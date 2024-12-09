d44f4475f5472fb7271001b47cc1af8887a677fb
Nico Marcuard <Nico_Marcuard@sluz.ch>
2024-12-09 08:27:58 +0100

    go4-ex-1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,32 @@
 package main
 
-// TODO: implement the function computeGrade
+import (
+	"fmt"
+)
+
+func computeGrade(gradePoints int) int {
+	if gradePoints >= 90 {
+		return 6
+	} else if gradePoints >= 70 && gradePoints < 90 {
+		return 5
+	} else if gradePoints >= 50 && gradePoints < 70 {
+		return 4
+	} else if gradePoints >= 30 && gradePoints < 50 {
+		return 3
+	} else if gradePoints >= 20 && gradePoints < 30 {
+		return 2
+	} else if gradePoints >= 0 && gradePoints < 20 {
+		return 1
+	}
+	return 1
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	//Mit gegeben wird die Punktzahl
+	fmt.Println(computeGrade(93)) //6
+	fmt.Println(computeGrade(83)) //5
+	fmt.Println(computeGrade(64)) //4
+	fmt.Println(computeGrade(39)) //3
+	fmt.Println(computeGrade(20)) //2
+	fmt.Println(computeGrade(2))  //1
 }
