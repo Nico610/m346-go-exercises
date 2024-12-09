0b0cdb80f0ffe1810bfcdfecfa0c8ec14fc6aa98
Nico Marcuard <Nico_Marcuard@sluz.ch>
2024-12-09 08:35:42 +0100

    go4-ex-2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,18 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+func computeHypotenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(4, 5))
+	fmt.Println(computeHypotenuse(8, 10))
+	fmt.Println(computeHypotenuse(34, 51))
+	fmt.Println(computeHypotenuse(7, 5.75))
+
 }
