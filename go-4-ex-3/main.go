2e11363c6195296789d46b9f4c17aa99572fcb0e
Nico Marcuard <Nico_Marcuard@sluz.ch>
2024-12-15 11:33:20 +0100

    go-4-ex-3

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,23 @@
 package main
 
-// TODO: implement the function computeQuadraticFormula
+import (
+	"fmt"
+	"math"
+)
+
+func computeQuadraticFormula(a, b, c float64) (float64, float64, error) {
+	diskriminant := math.Pow(b, 2) - 4*a*c
+
+	if diskriminant < 0 {
+		return 0, 0, fmt.Errorf("Fehler Ausgabe")
+	}
+
+	x1 := (-b + math.Sqrt(diskriminant)) / (2 * a)
+	x2 := (-b - math.Sqrt(diskriminant)) / (2 * a)
+
+	return x1, x2, nil
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(1, 5, 6))
 }
